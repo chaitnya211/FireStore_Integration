923f640e9d2c0ff5ce577eda72da2c62ada76f4f
Chaitnya <shelkecb@gmail.com>
2024-04-30 14:18:50 +0530

    created splash screen

--- lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
+++ lib/src/features/authentication/screens/on_boarding/on_boarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:login_firebase_complete/src/constants/colors.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -9,7 +10,20 @@ class OnBoardingScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          LiquidSwipe(
+            pages: [
+              Container(
+                color: tOnBoardingPage1Color,
+                child: Column(
+                  children: [
 
+                  ],
+                ),
+              ),
+              Container(color: tOnBoardingPage2Color),
+              Container(color: tOnBoardingPage3Color),
+            ],
+          )
         ],
       ),
     );
